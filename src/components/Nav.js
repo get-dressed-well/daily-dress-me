c066350cf19ac19a6e4f1098a1550d565e7e269f
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-01-04 16:19:45 -0500

    added axios to get weather

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,7 @@ class Nav extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showModal:  {display: "none"},
+      showModal: { display: 'none' },
       zipcode: '90210'
     };
 
@@ -22,57 +22,82 @@ class Nav extends Component {
 
   handleSubmit(event) {
     alert('A name was submitted: ' + this.state.zipcode);
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/zip=${
+          this.state.zipcode
+        }&APPID={APIKEY}`
+      )
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
     event.preventDefault();
   }
 
   openModel() {
-    this.setState({ showModal:  {display: "block"} });
+    this.setState({ showModal: { display: 'block' } });
   }
 
   closeModel() {
-    this.setState({ showModal:  {display: "none"} });
+    this.setState({ showModal: { display: 'none' } });
   }
 
   componentDidMount() {
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = (event) => {
-      console.log(event.target)
-        if (event.target.className === "middle") {
-          this.setState({ showModal:  {display: "none"} });
-        }
-    }
+    window.onclick = event => {
+      console.log(event.target);
+      if (event.target.className === 'middle') {
+        this.setState({ showModal: { display: 'none' } });
+      }
+    };
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.openModel} id="bars-btn"><i className="fas fa-bars"></i></button>
+        <button onClick={this.openModel} id="bars-btn">
+          <i className="fas fa-bars" />
+        </button>
         <div id="myModal" className="modal" style={this.state.showModal}>
-        <div className="outer">
-        <div className="middle">
-        <div className="inner">
-          <div className="modal-content">
-            <h3>Choose location</h3>
-            <span className="close" onClick={this.closeModel}>&times;</span>
-              <form onSubmit={this.handleSubmit} >
-              <label className="form-group">
-                Zipcode:
-                <input className="form-field" type="text" value={this.state.zipcode} onChange={this.handleChange}/>
-              </label>
-              <label className="form-group">
-                Gender:
-                <select className="form-field">
-                  <option>Male</option>
-                  <option>Female</option>
-                </select>
-              </label>
-              <input className="form-field modal-form-btn" type="submit" value="Save" />
-            </form>
+          <div className="outer">
+            <div className="middle">
+              <div className="inner">
+                <div className="modal-content">
+                  <h3>Choose location</h3>
+                  <span className="close" onClick={this.closeModel}>
+                    &times;
+                  </span>
+                  <form onSubmit={this.handleSubmit}>
+                    <label className="form-group">
+                      Zipcode:
+                      <input
+                        className="form-field"
+                        type="text"
+                        value={this.state.zipcode}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+                    <label className="form-group">
+                      Gender:
+                      <select className="form-field">
+                        <option>Male</option>
+                        <option>Female</option>
+                      </select>
+                    </label>
+                    <input
+                      className="form-field modal-form-btn"
+                      type="submit"
+                      value="Save"
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        </div>
-      </div>
-      </div>
       </div>
     );
   }
