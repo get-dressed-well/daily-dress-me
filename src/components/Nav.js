32d5676dcbaeff63287ff7dce97a8a51ea89ea6a
Dan Volosnikov <danvolosnikov@gmail.com>
2018-01-04 15:37:31 -0500

    added fonts and changed modal to react-modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,10 +4,15 @@ import '../styles/Nav.css';
 class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = {zipcode: '90210'};
+    this.state = {
+      showModal:  {display: "none"},
+      zipcode: '90210'
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.openModel = this.openModel.bind(this);
+    this.closeModel = this.closeModel.bind(this);
   }
 
   handleChange(event) {
@@ -15,34 +20,23 @@ class Nav extends Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + typeof this.state.zipcode);
+    alert('A name was submitted: ' + this.state.zipcode);
     event.preventDefault();
   }
 
+  openModel() {
+    this.setState({ showModal:  {display: "block"} });
+  }
+
+  closeModel() {
+    this.setState({ showModal:  {display: "none"} });
+  }
+
   componentDidMount() {
-    // Get the modal
-    var modal = document.getElementById('myModal');
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("bars-btn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
+    window.onclick = (event) => {
+        if (event.target.className === "middle") {
+          this.setState({ showModal:  {display: "none"} });
         }
     }
   }
@@ -50,14 +44,14 @@ class Nav extends Component {
   render() {
     return (
       <div>
-        <button id="bars-btn"><i className="fas fa-bars"></i></button>
-        <div id="myModal" className="modal">
+        <button onClick={this.openModel} id="bars-btn"><i className="fas fa-bars"></i></button>
+        <div id="myModal" className="modal" style={this.state.showModal}>
         <div className="outer">
         <div className="middle">
         <div className="inner">
           <div className="modal-content">
             <h3>Choose location</h3>
-            <span className="close">&times;</span>
+            <span className="close" onClick={this.closeModel}>&times;</span>
               <form onSubmit={this.handleSubmit} >
               <label className="form-group">
                 Zipcode:
@@ -70,7 +64,7 @@ class Nav extends Component {
                   <option>Female</option>
                 </select>
               </label>
-              <input className="form-field" type="submit" value="Save" />
+              <input className="form-field modal-form-btn" type="submit" value="Save" />
             </form>
           </div>
         </div>
