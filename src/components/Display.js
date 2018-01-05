95c0353ba5cfad322c159e394c868370ab3211cd
Dan Volosnikov <myrovoy@mail.ru>
2018-01-05 10:43:19 -0500

    random

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Display from React.Component {
+class Display extends React.Component {
   render() {
     return (
       <div style={{display: "flex", flexFlow: "column", alignItems: "center", justifyContent: "space-around", height: "200px", width: "400px", margin: "30px", borderRadius: "10px", background: "rgba(255, 255, 255, 0.6)"}}>
