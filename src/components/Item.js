e4b54d10759778513828df8622b05d73c8caf707
JasonCorp84 <csababalogh84@gmail.com>
2018-01-05 12:02:40 -0800

    cold weather warm weather

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-class Item extends React.Component {
-  render() {
-    return (
-      <div style={{display: "flex", padding: "10px", height: "200px", borderRadius: "10px", background: "rgba((255, 255, 255, 0.5)", boxShadow: "2px 3px #888888", marginBottom: "10px"}}>
-        <img style={{width:"200px"}} src={this.props.cItem.image} />
-=======
 import React from 'react';
 
 class Item extends React.Component {
   render() {
     return (
->>>>>>> 17225e6247006827fc328e29e8dbe812fa47cee7
       <div
         style={{
           display: 'flex',
@@ -22,19 +14,9 @@ class Item extends React.Component {
           marginBottom: '10px',
           justifyContent: 'space-between'
         }}
-<<<<<<< HEAD
-      
-        <img style={{ width: '200px' }} src={this.props.cItem.image} alt="" />
-        {/* <div style={{width: "100px", height: "100%", border: "1px solid black"}}></div> */}
-        <div style={{display: "flex", flexFlow: "column", alignItems: "left", marginLeft: "20px"}}>
-          <p style={{color: "#2a68cc"}}>{this.props.cItem.name}</p>
-          <p style={{color: "#2a68cc"}}>{this.props.cItem.company}</p>
-          <p style={{color: "#2a68cc"}}>{this.props.cItem.description}</p>
-=======
       >
         <img style={{ width: '200px' }} src={this.props.cItem.image} alt="" />
         {/* <div style={{width: "100px", height: "100%", border: "1px solid black"}}></div> */}
->>>>>>> 17225e6247006827fc328e29e8dbe812fa47cee7
         <div
           style={{
             display: 'flex',
@@ -42,19 +24,11 @@ class Item extends React.Component {
             alignItems: 'left',
             marginLeft: '20px'
           }}
-<<<<<<< HEAD
-        
-=======
         >
->>>>>>> 17225e6247006827fc328e29e8dbe812fa47cee7
           <p style={{ color: '#2a68cc' }}>{this.props.cItem.name}</p>
           <p style={{ color: '#2a68cc' }}>{this.props.cItem.company}</p>
           <p style={{ color: '#2a68cc' }}>{this.props.cItem.description}</p>
         </div>
-<<<<<<< HEAD
-        <p style={{color: "#2a68cc"}}><a href={this.props.cItem.link}>more</a></p>
-=======
->>>>>>> 17225e6247006827fc328e29e8dbe812fa47cee7
         <p style={{ color: '#2a68cc' }}>
           <a href={this.props.cItem.link}>more</a>
         </p>
@@ -62,10 +36,5 @@ class Item extends React.Component {
     );
   }
 }
-<<<<<<< HEAD
- 
-export default Item;
-=======
 
 export default Item;
->>>>>>> 17225e6247006827fc328e29e8dbe812fa47cee7
--- src/components/outfitList.js
+++ src/components/outfitList.js
@@ -3,10 +3,20 @@ import React from 'react';
 import Outfit from './outfit.js';
 
 import coldOutfits from '../data/cold-weather-outfits.js';
+import warmWeatherOutfit from '../data/warm-weather-outfits.js';
 
 class OutfitList extends React.Component {
   mapOutfitsArray() {
-    return coldOutfits.map((outfit, index) => {
+    let temp = parseInt(this.props.temperature,10);
+    let outfitList;
+
+    if(temp > 60) {
+      outfitList = warmWeatherOutfit;
+    } else {
+      outfitList = coldOutfits;
+    }
+
+    return outfitList.map((outfit, index) => {
       return (
         <div key={index}>
           <p>Cold Weather Style {index + 1} Outfit</p>
