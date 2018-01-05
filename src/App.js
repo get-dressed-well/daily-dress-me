f8fd2e88299d4b66b6cce3679aa3bc39a0b18388
JasonCorp84 <csababalogh84@gmail.com>
2018-01-05 10:52:20 -0800

    e

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
+import OutfitsPage from './components/OutfitsPage';
 //import Main from './components/Main';
 import './styles/App.css';
-
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-        {/* <Main /> */}
-      </div>
-    );
-  }
-}
