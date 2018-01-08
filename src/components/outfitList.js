dcb587a6d063d578c30b35b1584d110356e27ea4
Sondro <memoiran@hotmail.com>
2018-01-08 07:10:11 -0800

    Add files via upload

---
+++ src/components/outfitList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Outfit from './outfit.js';
+
+import coldOutfits from '../data/cold-weather-outfits.js';
+import warmWeatherOutfit from '../data/warm-weather-outfits.js';
+
+class OutfitList extends React.Component {
+  mapOutfitsArray() {
+    let temp = parseInt(this.props.temperature,10);
+    let outfitList;
+
+    if(temp > 66) {
+      outfitList = warmWeatherOutfit;
+    } else {
+      outfitList = coldOutfits;
+    }
+
+    return outfitList.map((outfit, index) => {
+      return (
+        <div key={index}>
+          <p>Cold Weather Style {index + 1} Outfit</p>
+          <Outfit clothes={outfit.clothes} />
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ width: '80%', padding: '10px' }}>
+        {this.mapOutfitsArray()}
+      </div>
+    );
+  }
+}
+
+export default OutfitList;
